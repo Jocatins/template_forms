f6354bf8fd7960bb0ff01321438e25cfe29b3b48
Nicholas Agwah <nagwah@teqbridgeltd.com>
2020-12-06 17:46:11 +0100

    template_forms

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'test-app';
+
+  onSubmit(data) {
+    console.log(data);
+  }
+}
